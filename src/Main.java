252b286d4bb65be04aaf408a473439466086ce21
Ivan Trofimov <iam@ivan-trofimov.ru>
2019-09-10 15:46:47 +0700

    Прокомментирован код

--- src/Main.java
+++ src/Main.java
@@ -11,17 +11,24 @@ public class Main {
     private FileWriter output;
 
     public static void main(String[] args) throws java.io.IOException {
+        // Открытие файла input.txt
         FileReader input = new FileReader("input.txt");
         Scanner input_scan = new Scanner(input);
 
         int T = Integer.parseInt(input_scan.nextLine());
 
+        // Создание коллекций для хранения вершин и ребер графа
         Set<String> heads = new HashSet<>();
         Map<String, Integer> edges = new HashMap<>();
 
+        // Создание переменных для хранения строки S и подстрок-вершин графа subS
         String S;
         String subS1, subS2;
 
+        // Перебираем все исходные строки S, разбивая их на триады subS.
+        // Триады добавляются в коллекцию уникальных вершин (Set) heads.
+        // Две идущие подряд вершины образуют ребро и добавляются в Map edges, где ключом выступает строка с названиеми
+        // обеих вершин, а в качестве значения - их количество (вес).
         for(int i=1; i<=T; i++){
             S=input_scan.nextLine();
             subS1=S.substring(0,3);
@@ -30,7 +37,7 @@ public class Main {
                 subS2=S.substring(j, j+3);
                 heads.add(subS2);
                 String key = subS1 + " " + subS2;
-                if(edges.containsKey(key)){
+                if(edges.containsKey(key)){ // Проверка на наличие такого ключа в Map
                     edges.replace(key, edges.get(key)+1);
                 }else{
                     edges.put(key, 1);
@@ -38,18 +45,23 @@ public class Main {
                 subS1=subS2;
             }
         }
+
+        // Закрываем файл input.txt
         input_scan.close();
         input.close();
 
+        // Открываем файл output.txt на запись
         FileWriter output = new FileWriter("output.txt");
 
-        output.write(String.valueOf(heads.size())+"\n");
-        output.write(String.valueOf(edges.size())+"\n");
+        // Выводим количество вершин, количество ребер графа и все ребра с количеством повторов (весом)
+        output.write(heads.size() +"\n");
+        output.write(edges.size() +"\n");
 
         for(Map.Entry<String, Integer> edge : edges.entrySet()){
             output.write(edge.getKey()+ " " + edge.getValue()+"\n");
         }
 
+        // Закрываем файл output.txt
         output.close();
     }
 }
